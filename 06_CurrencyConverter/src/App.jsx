866ea622a6ee9f27228f322d33ee35902d898214
rf_104 <kingaref13@gmail.com>
2024-06-14 12:19:34 +0600

    changed

--- 06_CurrencyConverter/src/App.jsx
+++ 06_CurrencyConverter/src/App.jsx
@@ -7,28 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage:`url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=600)`}}>
+        <h1 className='bg-yellow-400 text-clip text-3xl p-2 rounded-md text-center'>This is Test</h1>
+    </div>
   )
 }
 
